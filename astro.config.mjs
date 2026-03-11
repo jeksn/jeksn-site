49f6a31b210d1dc1299e93763cdd899c58a0e12a
Johan <johan@jeksn.me>
2026-03-11 14:36:13 +0100

    chore: fix build errors after astro 6 update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwind from '@astrojs/tailwind';
-
 import react from '@astrojs/react';
 
 import alpinejs from '@astrojs/alpinejs';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), alpinejs()],
+  integrations: [react(), alpinejs()],
   redirects: {
 	'/small-projects': '/projects',
 	'/blog': '/',
